2c4bc14b1b4258e37fa81f5c95479cb6c20fb7fa
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-11-12 10:03:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,5 +4,6 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {['Phil Ochs']: "Here's to the State of Mississippi"})
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi";
+  return playlist;
 }
